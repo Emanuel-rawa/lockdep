116a35fc6498ecdf72ef0b230de9857d201e39e1
Vinicius de Lima <vldsampaio@pm.me>
2025-07-12 17:58:39 -0300

    feat: add lock registry to identify interposed locks
    Co-authored-by: Yuri Santos <yurimax322@gmail.com>

--- src/lockdep/lockdep_core.c
+++ src/lockdep/lockdep_core.c
@@ -1,14 +1,44 @@
-#include <execinfo.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "lockdep.h"
 
 bool lockdep_enabled = true;
 
-void lockdep_init(void) {
+static lock_node_t* lock_registry;
+static pthread_mutex_t lockdep_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void* smalloc(const size_t bytes)
+{
+    void* ptr = malloc(bytes);
+    if (!ptr) {
+        fprintf(stderr, "out of memory, aborting\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+static lock_node_t* find_or_create_lock(const void* lock_addr)
+{
+    lock_node_t* lock = lock_registry;
+
+    while (lock) {
+        if (lock->lock_addr == lock_addr)
+            return lock;
+        lock = lock->next;
+    }
+
+    lock = smalloc(sizeof(lock_node_t));
+    lock->children = NULL;
+    lock->lock_addr = lock_addr;
+    lock->next = lock_registry;
+    return lock_registry = lock;
+}
+
+void lockdep_init(void)
+{
     const char* env = getenv("LOCKDEP_DISABLE");
     if (env && strcmp(env, "1") == 0) {
         lockdep_enabled = false;
@@ -18,16 +48,25 @@ void lockdep_init(void) {
     fprintf(stderr, "[LOCKDEP] Lockdep initialized\n");
 }
 
-void lockdep_cleanup(void) {
-    // TODO: Implement if needed
-    return;
+bool lockdep_acquire_lock(const void* lock_addr)
+{
+    printf("[LOCKDEP] Acquiring lock  %p\n", lock_addr);
+
+    pthread_mutex_lock(&lockdep_mutex);
+
+    lock_node_t* lock = find_or_create_lock(lock_addr);
+
+    pthread_mutex_unlock(&lockdep_mutex);
+
+    return false;
 }
 
-bool lockdep_acquire_lock(const void* lock_addr) {
-    printf("[LOCKDEP] Acquiring lock at %p\n", lock_addr);
-    return true;
-}
+void lockdep_release_lock(const void* lock_addr)
+{
+    printf("[LOCKDEP] Releasing lock %p\n", lock_addr);
 
-void lockdep_release_lock(const void* lock_addr) {
-    printf("[LOCKDEP] Releasing lock at %p\n", lock_addr);
+    pthread_mutex_lock(&lockdep_mutex);
+
+
+    pthread_mutex_unlock(&lockdep_mutex);
 }
